0da3a22a2d8252c225e071b0fb92726f4af688cd
Thayrine <thayrinerenata@gmail.com>
2022-02-01 20:43:06 -0300

    arrumando

---
+++ src/main/java/org/generation/bloguinho/BloguinhoApplication.java
@@ -0,0 +1,13 @@
+package org.generation.bloguinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloguinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloguinhoApplication.class, args);
+	}
+
+}
