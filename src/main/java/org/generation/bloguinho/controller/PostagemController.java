6ff0cfe683a709691e122cc870daf99675b97155
Thayrine <thayrinerenata@gmail.com>
2022-02-05 20:10:15 -0300

    exercício 2

--- src/main/java/org/generation/bloguinho/controller/PostagemController.java
+++ src/main/java/org/generation/bloguinho/controller/PostagemController.java
@@ -1,13 +1,13 @@
 package org.generation.bloguinho.controller;
 
 import java.util.List;
-
 import org.generation.bloguinho.models.Postagem;
 import org.generation.bloguinho.repositories.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,6 +48,11 @@ public class PostagemController {
 	@PutMapping
 	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem) {
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 
 }
--- src/main/java/org/generation/bloguinho/models/Postagem.java
+++ src/main/java/org/generation/bloguinho/models/Postagem.java
@@ -5,9 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 
 @Entity
 @Table(name= "postagens")
@@ -17,16 +22,21 @@ public class Postagem {
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private long id;
 	
-	//@NotNull
-	//@Size(min = 5, max = 100)
+	@NotNull
+	@Size(min = 5, max = 100)
 	private String titulo;
 	
-	//@NotNull
-	//@Size(min = 10, max = 500)
+	@NotNull
+	@Size(min = 10, max = 500)
 	private String texto;
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data= new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	
 	public long getId() {
 		return id;
@@ -51,6 +61,13 @@ public class Postagem {
 	}
 	public void setData(Date data) {
 		this.data = data;
+	}
+	
+	public Tema getTema() {
+		return tema;
+	}
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 }
